c8370a11c7af1e739b6f9c7a27f667e78f2a6f4d
Luke Rogerson <luke.rogerson@stint.co>
2021-05-02 22:35:17 +0100

    Simplify lambda deployment setup

--- Makefile
+++ Makefile
@@ -1,3 +1,7 @@
 deploy:
+	@rm lambda.zip || true
+	@pip install -r ./app/lambda/requirements.txt -t ./app/lambda/deps
+	@cd ./app/lambda/deps && zip -r ../../../lambda.zip .
+	@cd ./app/lambda && zip -g ../../lambda.zip main.py lib
 	@cd terraform && terraform apply -var-file=vars.tfvars -auto-approve
 	@tput setaf 2; echo "Deployed successfully!"
--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,7 +1,3 @@
-# https://stackoverflow.com/a/62065323
-# https://www.davidbegin.com/the-most-minimal-aws-lambda-function-with-python-terraform/
-# https://medium.com/rockedscience/hard-lessons-from-deploying-lambda-functions-with-terraform-4b4f98b8fc39
-
 locals {
   lambda_src_path = "../app/lambda"
 }
@@ -17,30 +13,13 @@ resource "random_uuid" "lambda_src_hash" {
   }
 }
 
-resource "null_resource" "install_dependencies" {
-  provisioner "local-exec" {
-    command = "pip install -r ${local.lambda_src_path}/requirements.txt -t ${local.lambda_src_path}/ --upgrade"
-  }
-  triggers = {
-    source_code_hash = random_uuid.lambda_src_hash.result # This is a suitable option too
-  }
-}
-
-data "archive_file" "lambda_zip" {
-  type        = "zip"
-  source_dir  = "../app/lambda"
-  output_path = "../lambda.zip"
-  depends_on = [null_resource.install_dependencies]
-}
-
-
 resource "aws_lambda_function" "lambda_function" {
   role             = aws_iam_role.feats_dynamo_role.arn
   handler          = var.handler
   runtime          = var.runtime
-  filename         = data.archive_file.lambda_zip.output_path
+  filename         = "../lambda.zip"
+  source_code_hash = random_uuid.lambda_src_hash.result 
   function_name    = var.function_name
-  source_code_hash = data.archive_file.lambda_zip.output_base64sha256 
 }
 
 resource "aws_iam_role_policy" "feats_dynamo_policy" {
