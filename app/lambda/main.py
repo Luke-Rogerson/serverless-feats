edb4324129df9f1adafa74a3814917ceecf7ce3d
Luke Rogerson <luke.rogerson@stint.co>
2021-05-03 15:10:26 +0100

    Add Flask

--- app/lambda/main.py
+++ app/lambda/main.py
@@ -1,39 +1,30 @@
-import json
-from typing import Any, Dict, Optional, TypedDict, List, Union, cast
 # from lib import user
+from flask import Flask, jsonify
+import serverless_wsgi
 
-from aws_lambda_types.api_gw import (
-    APIGWPayloadV1RequestContextDict,
-    APIGWPayloadV1RequestDict,
-    APIGWPayloadV1ResponseDict,
-)
-class CreateUser(TypedDict):
-    username: str
-    email: str
-    interests: List[str]
-    based_in: str
+# class CreateUser(TypedDict):
+#     username: str
+#     email: str
+#     interests: List[str]
+#     based_in: str
 
-def lambda_handler(
-    event: APIGWPayloadV1RequestDict, context: APIGWPayloadV1RequestContextDict
-) -> APIGWPayloadV1ResponseDict:
-    
-    # remove prepended "dev"
-    path = event["requestContext"]["path"][4:]
-    return cast(APIGWPayloadV1ResponseDict, switch(path=path, event=event))
-
-def construct_success_response(body: Union[Dict[str, Any], APIGWPayloadV1RequestDict], status_code: Optional[int] = 200):
-    return {
-        "statusCode": status_code,
-        "headers": {
-            'Content-Type': 'application/json',
-        },
-        "body": json.dumps(body),
-    }   
+app = Flask(__name__)
 
 
-def switch(path: str, event: APIGWPayloadV1RequestDict):
-    if path == "/createuser":
-        return construct_success_response(body={"path": "createuser"})
-    elif path == "/createfeat":
-        return construct_success_response(body={"path": "createfeat"})
-    return construct_success_response(body=event)
+@app.route("/")
+def add():
+    return jsonify(result="test")
+
+
+@app.route("/create")
+def create():
+    return jsonify(create="test", feat="test")
+
+
+@app.route("/remove")
+def remove():
+    return jsonify(remove="test", feat="test")
+
+
+def lambda_handler(event, context):
+    return serverless_wsgi.handle_request(app, event, context)
